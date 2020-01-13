eef78ec17173f3f2db75e3790eb69c9a16d91cc3
Semesse <Semperia@outlook.com>
2020-01-13 18:19:00 +0800

    configured webpack and tsconfig for aliases

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const glob = require('glob');
-const path = require('path');
+const glob = require('glob')
+const path = require('path')
+const merge = require('webpack-merge')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -7,15 +8,15 @@ function resolve (dir) {
 
 function scanEntries() {
   let entryPath = 'src/views/*/index.html'
-  let entries = {};
+  let entries = {}
   glob.sync(entryPath).forEach(function(entry) {
     let filename = /src\/views\/(\S+)\/index.html/.exec(entry)[1]
-      entries[filename] = {
-        entry: 'src/views/' + filename + '/index.ts',
-        template: entry,
-        filename: filename,
-      chunks: ["chunk-vendors", "chunk-common", filename]
-    };
+    entries[filename] = {
+      entry: 'src/views/' + filename + '/index.ts',
+      template: entry,
+      filename: filename,
+    chunks: ["chunk-vendors", "chunk-common", filename]
+    }
     if (process.env.NODE_ENV === "production") {
       entries[filename] = merge(entries[filename], {
       minify: {
@@ -24,28 +25,27 @@ function scanEntries() {
         removeAttributeQuotes: true
       },
       chunksSortMode: "dependency"
-      });
+      })
     }
-  });
-  return entries;
+  })
+  return entries
 }
 
 module.exports = {
+  runtimeCompiler: true,
   lintOnSave: true,
   productionSourceMap: false,
   pages: scanEntries(),
   devServer: {
-    index: '/', 
+    index: '/index', 
     host: '0.0.0.0',
     port: 8081,
   },
   chainWebpack: (config)=>{
     config.resolve.alias
       .set('@$', resolve('src'))
-      .set('assets',resolve('src/assets'))
-      .set('components',resolve('src/components'))
-      .set('index',resolve('src/views/index'))
-      .set('admin',resolve('src/views/admin'))
-      .set('static',resolve('src/static'))
+      .set('~$', resolve('src/common'))
+      .set('index', resolve('src/views/index'))
+      .set('admin', resolve('src/views/admin'))
   }
 };
